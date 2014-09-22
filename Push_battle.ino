cc5f699d1fd45d3ef8bb3b33c34bf884b52cacf6
kkmonster <kkmonster@hotmail.com>
2014-09-22 16:16:45 +0700

    remove comment

--- Push_battle.ino
+++ Push_battle.ino
@@ -8,9 +8,7 @@
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      30
 #define btness      150
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
+
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 int delayval = 500; // delay for half a second
